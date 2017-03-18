779eaf87b000328246c07bc58dc9dc16c24d32a9
yinhuan <ykzzldx269@163.com>
2017-03-18 11:48:42 +0800

    commit.

---
+++ app/src/main/java/com/yinhuan/imageloader/MainActivity.java
@@ -0,0 +1,18 @@
+package com.yinhuan.imageloader;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
